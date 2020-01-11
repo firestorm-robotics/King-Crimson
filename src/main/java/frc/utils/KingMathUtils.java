7f357335fc2b0efb8f05eda32f7c0ad112360224
Andrew-Cua <andrew@cuafamily.com>
2020-01-11 10:49:41 -0500

    more quality changes

--- src/main/java/frc/utils/KingMathUtils.java
+++ src/main/java/frc/utils/KingMathUtils.java
@@ -2,36 +2,37 @@ package frc.utils;
 
 public class KingMathUtils {
     public static double logit(double x) {
-        if(x == 0) {
-          return x;
-        }else if(x < 0) {
-          x = Math.abs(x);
-          x = (0.15*Math.log((x/(1-0.999535*x)))+0.5)/2;//(0.15*Math.log((-x+1)/(1-(0.999535*(-x+1))))+0.5);
-          x = -x;
-        }else {
-          x = 0.15*Math.log((x/(1-0.999535*x)))+0.5;
+        double scaledX = 0;
+        if (x == 0) {
+            return 0;
+        } else if (x < 0) {
+            scaledX = Math.abs(x);
+            scaledX = (0.15 * Math.log((scaledX / (1 - 0.999535 * scaledX))) + 0.5) / 2;// (0.15*Math.log((-x+1)/(1-(0.999535*(-x+1))))+0.5);
+            scaledX = Math.copySign(scaledX, x);
+        } else {
+            scaledX = 0.15 * Math.log((scaledX / (1 - 0.999535 * scaledX))) + 0.5;
         }
-        
+
         return x;
     }
 
     public static double turnExp(double x) {
-        double xSign = x;
-        if(x == 0) {
-            return x;
-        }else if( Math.abs(x) < 0.3) {
-            x = 4*Math.pow(x,2);
-        } else if(Math.abs(x) >= 0.3 && Math.abs(x) < 0.6) {
-            x = 0.4*Math.pow(Math.abs(x),0.1);
-        }else if(Math.abs(x) >= 0.6) {
-            x = 0.6*Math.pow((Math.abs(x)-0.6),0.1);
+        double scaledX = 0;
+        if (x == 0) {
+            return 0;
+        } else if (Math.abs(x) < 0.3) {
+            scaledX = 4 * Math.pow(x, 2);
+        } else if (Math.abs(x) >= 0.3 && Math.abs(x) < 0.6) {
+            scaledX = 0.4 * Math.pow(Math.abs(x), 0.1);
+        } else if (Math.abs(x) >= 0.6) {
+            scaledX = 0.6 * Math.pow((Math.abs(x) - 0.6), 0.1);
         }
-        x = Math.copySign(x, xSign);
-        return x;
+        scaledX = Math.copySign(scaledX, x);
+        return scaledX;
     }
 
     public static double clampD(double x, double bounds) {
-        if(Math.abs(x) <= bounds) {
+        if (Math.abs(x) <= bounds) {
             return 0;
         }
         return x;
