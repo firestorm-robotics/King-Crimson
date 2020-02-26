58049c6fc5a34700fa2b13c5dfdfe866e6b1fae9
LinuxRocks2000 <you@example.com>
2020-02-26 18:47:23 -0500

    f

---
+++ src/main/java/frc/utils/FunctionButton.java
@@ -0,0 +1,21 @@
+package frc.utils;
+
+class FunctionButton{
+    protected boolean swit;
+    public FunctionButton(){
+        this.swit=false;
+    }
+    public void stateIsTrue(){
+        if (this.swit==false){
+            this.swit=true;
+            trigger();
+        }
+    }
+    public void stateIsFalse(){
+        if (this.swit==true){
+            this.swit=false;
+        }
+    }
+    public void trigger(){};
+}
+// Extend this class for calling functions once, and only once, upon condition equals true.
